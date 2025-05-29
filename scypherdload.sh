1a9a41d7f978f8db28bf4bfd87a8eaacda671460
Patato55 <109925980+moon-miner@users.noreply.github.com>
2025-05-29 04:01:28 -0300

    Update scypherdload.sh
    Updated to SCypherV2 Ergohack release.

--- scypherdload.sh
+++ scypherdload.sh
@@ -1,37 +1,40 @@
- 
 #!/bin/bash
 
-# URLs de la API
+# API URLs
 urls=(
-    "https://api.sigmaspace.io/api/v1/tokens/e19e95429dc4566292ddda535129aa5e3c0b31d9139814a52e21508510a9b389"
-    "https://api.sigmaspace.io/api/v1/tokens/01efc34d6752de5554d1d756b1fba3da094e41f8cf59904902ed90ec3e18ef43"
-    "https://api.sigmaspace.io/api/v1/tokens/0f19789154bf43d1cb7cb9deb8e1603adc614506ae75e95b827b7b8c65dbb753"
-    "https://api.sigmaspace.io/api/v1/tokens/aca4dd9bcd304e3bd32d21e477d7b3b840f7d50ebb85b7a6ab1cc483a7ebaa9a"
+    "https://api.sigmaspace.io/api/v1/tokens/01594be725214d0282e154991b1797fdf306a99ccc624b4642d40f387afe4a4f"
+    "https://api.sigmaspace.io/api/v1/tokens/6a6c1307a1e4a7c5e72126aa19846c8f8c81d59f3d75f4e42b60e1efd3d49265"
+    "https://api.sigmaspace.io/api/v1/tokens/3a2a80cb9ca428a90b9dbe9b2ef61a41f8b868a233b702d2090dcea466de28bc"
+    "https://api.sigmaspace.io/api/v1/tokens/154327dc6767265842e996e9cdf4f232b62733b21c337b1e041c5255fb73d967"
+    "https://api.sigmaspace.io/api/v1/tokens/a3cccedd94e11579ccb299acc758aab945a6ce9447ad168d3325280ae1795e29"
+    "https://api.sigmaspace.io/api/v1/tokens/63b056564a399cfcad7437d36023a72bc8d81b64e97a0615bf2950f98cd2952f"
+    "https://api.sigmaspace.io/api/v1/tokens/1efa48085300bfbd94f30ad6ed0dfcc51448f2dbec544fd391ef7c6f0f64a949"
+    "https://api.sigmaspace.io/api/v1/tokens/3962e719f65b9c5d540727fe59ebc9dd62b93ae43c059e20a95a2d377f91b674"
 )
 
-# Archivo temporal para almacenar el contenido base64
+# Temporary file to store base64 content
 temp_base64="scypherBASE64.txt"
-# Limpiar el archivo temporal si existe
+# Clear the temporary file if it exists
 > "$temp_base64"
 
-# Procesar cada URL
+# Process each URL
 for url in "${urls[@]}"; do
-    echo "Procesando $url..."
+    echo "Processing $url..."
 
-    # Realizar la petición curl y extraer el campo description usando jq
+    # Make curl request and extract description field using jq
     description=$(curl -s -X GET "$url" | jq -r '.description')
 
-    # Añadir el contenido al archivo base64
+    # Add content to base64 file
     echo "$description" >> "$temp_base64"
 done
 
-echo "Decodificando base64 a archivo comprimido..."
-base64 -d "$temp_base64" > scypher.sh.xz
+echo "Decoding base64 to compressed file..."
+base64 -d "$temp_base64" > SCypherV2.sh.xz
 
-echo "Descomprimiendo archivo xz..."
-xz -d scypher.sh.xz
+echo "Decompressing xz file..."
+xz -d SCypherV2.sh.xz
 
-echo "Limpiando archivos temporales..."
+echo "Cleaning temporary files..."
 rm "$temp_base64"
 
-echo "Proceso completado. El script resultante es scypher.sh"
+echo "Process completed. The resulting script is SCypherV2.sh"
